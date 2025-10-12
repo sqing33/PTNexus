002392cc667dc3263a842c99d6c62f12af31eb57
sqing <1395251710@qq.com>
2025-10-12 12:34:07 +0800

    新增：iyuu缓存 修复：mysql数据库错误 新增：转种目标站点

--- batch-enhancer/main.go
+++ batch-enhancer/main.go
@@ -137,7 +137,8 @@ type BencodeParser struct {
 var (
 	coreAPIURL     = getEnv("CORE_API_URL", "http://localhost:35274")
 	port           = getEnv("PORT", "35273")
-	tempDir        = getEnv("TEMP_DIR", "/app/data/tmp")
+	// tempDir        = getEnv("TEMP_DIR", "/app/data/tmp")
+	tempDir        = getEnv("TEMP_DIR", "/app/Code/Dockerfile/Docker.pt-nexus/server/data/tmp")
 	internalSecret = getEnv("INTERNAL_SECRET", "pt-nexus-2024-secret-key") // 共享密钥，用于生成动态token
 )
 
